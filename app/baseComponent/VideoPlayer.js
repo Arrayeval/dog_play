ba0b1089c5aef4962a52d20a9b8c3091b208100b
evel.yi <evel.yi@yff.com>
2018-11-26 18:37:17 +0800

    VideoPlayer.js

--- app/baseComponent/VideoPlayer.js
+++ app/baseComponent/VideoPlayer.js
@@ -2,6 +2,13 @@
 import React, {PureComponent} from 'react'
 import {View, Text, StyleSheet, TouchableOpacity,　Image, Dimensions} from 'react-native'
 
+import PropsTypes from 'prop-types'
+import Video from 'react-native-video'
+import Orientation from 'react-native-orientation'
+
+const screenWidth = Dimensions.get('window').width
+const defaultVideoHeight = screenWidth * 9 / 16
+
 export default class VideoPlayer extends React.Component {
   constructor (props) {
     super(props)
